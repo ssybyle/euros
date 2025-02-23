73852875f4b3abaab7429096a9ec9b934a81e55a
ssybyle <123334121+ssybyle@users.noreply.github.com>
2025-02-23 19:06:22 +0100

    Update main.py

--- server/main.py
+++ server/main.py
@@ -145,4 +145,4 @@ if __name__ == "__main__":
 
     print("- Removed old uploaded/result files")
 
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
